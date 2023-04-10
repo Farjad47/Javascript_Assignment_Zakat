b64c63686926ec41d9b10452c2b8cd0850df4717
Farjad47 <farjadahmed47@gmail.com>
2023-04-10 15:46:47 +0500

    FEAT Zakat Program

---
+++ zakat.js
@@ -0,0 +1,11 @@
+// 03-April-2023
+// JS Task-1
+// Zakat Program
+
+console.log("Welcome to the Zakat Program");
+let zakatPercentage = 2.5/100;
+let userInput = +prompt("Please enter your Amount to be calculated w.r.t Zakat:-");
+let result = userInput*zakatPercentage;
+
+alert("Your Zakat Value is " + result);
+
